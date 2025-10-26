3a3c17d9afce5b7215d708f6a483c34edf50230d
James Willock <james.willock@gmail.com>
2025-10-26 08:48:06 +0000

    Test coverage for roundToTwo

--- test/utilities.test.ts
+++ test/utilities.test.ts
@@ -1,6 +1,10 @@
-import { filledArray } from "../utilities.ts";
+import { filledArray, roundToTwo } from "../utilities.ts";
 import { assertEquals } from "https://deno.land/std@0.113.0/testing/asserts.ts";
 
 Deno.test("filledArray: returns an incrementing collection", () => {
   assertEquals(filledArray(5), [0, 1, 2, 3, 4]);
+});
+
+Deno.test("roundToTwo: returns a number to two decimal places", () => {
+  assertEquals(roundToTwo(1.11111111), 1.11);
 });
